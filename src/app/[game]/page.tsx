eed2a059e4a81d8388b4e27623f000cd4a3d0d52
Utku Bilgin <h.utkubilgin@gmail.com>
2024-05-20 06:10:12 +0300

    PageProps

--- src/app/[game]/page.tsx
+++ src/app/[game]/page.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import GameContainer from "@/containers/game/index"
-
 import { notFound } from "next/navigation";
-
 import { fetchLanguage } from "@/services/game"
 import { LanguageData } from "@/app/type/LanguageData";
 
-import { fetchIsWord } from "@/services/game";
 
 interface PageProps {
-  params: {
-    game: string;
-  };
+  params: any;
   pageLanguage: LanguageData;
 }
 
@@ -24,11 +19,6 @@ const Page: React.FC<PageProps> = async ({ params }) => {
 
   if(params.game !== "tr" && params.game !== "en")
     notFound()
-  
-    // if (searchParams.error === "true") {
-    //   throw new Error("Something went wrong!");
-    // }
-    // bunuda params ın yanına parametre olarak vericez ve backend ten error gelmesi durumunda error sayfasına yönlendirecek
 
 
   return (
